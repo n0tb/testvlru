88252c74298113c311e303c084c032c43a37433f
Yagodkin Igor <yagodkinigor@yandex.ru>
2018-08-10 12:44:32 +1000

    Add project files.

---
+++ TestVlru/TestCompanySearch.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace TestVlru
+{
+    [TestClass]
+    public class TestCompanySearch
+    {
+        private static ChromeDriver driver;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            driver.Navigate().GoToUrl(@"https://www.vl.ru");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("vl_input")));
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Correct_Value()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys("днс");
+            input.Submit();
+
+            var companies = driver.FindElementsByClassName("company__header");
+            var oneCompany = companies.First();
+
+            Assert.AreEqual("DNS", oneCompany.Text);
+            Console.WriteLine(oneCompany.Text);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Space_In_Value()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys(" днс ");
+            input.Submit();
+
+            var companies = driver.FindElementsByClassName("company__header");
+            var oneCompany = companies.First();
+
+            Assert.AreEqual("DNS", oneCompany.Text);
+            Console.WriteLine(oneCompany.Text);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_In_Uppercase()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys("ДНС");
+            input.Submit();
+
+            var companies = driver.FindElementsByClassName("company__header");
+            var oneCompany = companies.First();
+
+            Assert.AreEqual("DNS", oneCompany.Text);
+            Console.WriteLine(oneCompany.Text);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_End_Dot()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys("днс.");
+            input.Submit();
+
+            var companies = driver.FindElementsByClassName("company__header");
+            var oneCompany = companies.First();
+
+            Assert.AreEqual("DNS", oneCompany.Text);
+            Console.WriteLine(oneCompany.Text);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_ContainsOf_Multiple_Word()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys("Центр профессиональной стоматологии");
+            input.Submit();
+
+            var companies = driver.FindElementsByClassName("company__header");
+            var oneCompany = companies.First();
+
+            Assert.AreEqual("Центр профессиональной стоматологии", oneCompany.Text);
+            Console.WriteLine(oneCompany.Text);
+        }
+
+        [TestMethod]
+        public void Unsuccessful_Search_With_Uncorrect_Value()
+        {
+            var input = driver.FindElementByClassName("vl_input");
+            input.SendKeys("УсыЛапыИХвост");
+            input.Submit();
+
+            var result = driver.FindElementByCssSelector("h1.search").Text;
+          
+            Assert.AreEqual("Ничего не найдено", result);
+            Console.WriteLine(result);
+        }
+
+        [ClassCleanup]
+        public static void Close()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ TestVlru/TestGlobalSearch.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace TestVlru
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static ChromeDriver driver;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext) {
+            driver = new ChromeDriver();
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            driver.Navigate().GoToUrl(@"https://www.vl.ru");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("header__search-form-toggle")));
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Correct_Value()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys("вакансии");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByClassName("item-title").Text;
+            Assert.AreEqual("Агентства по трудоустройству", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Space_In_Value()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys(" вакансии ");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByClassName("item-title").Text;
+            Assert.AreEqual("Агентства по трудоустройству", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_In_Mix_Register()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys("ВаКаНсИи");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByClassName("item-title").Text;
+            Assert.AreEqual("Агентства по трудоустройству", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_In_Uppercase()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys("ВАКАНСИИ");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByClassName("item-title").Text;
+            Assert.AreEqual("Агентства по трудоустройству", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [TestMethod]
+        public void Successful_Search_With_Value_End_Dot()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys("вакансии.");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByClassName("item-title").Text;
+            Assert.AreEqual("Агентства по трудоустройству", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [TestMethod]
+        public void Unsuccessful_Search_With_Uncorrect_Value()
+        {
+            var searchLink = driver.FindElementByClassName("header__search-form-toggle");
+            searchLink.Click();
+
+            var textArea = driver.FindElementByClassName("header__search-query");
+            textArea.SendKeys("Так щедро август звёзды расточал");
+            textArea.Submit();
+
+            var searchResult = driver.FindElementByCssSelector("h1.search").Text;
+            Assert.AreEqual("Ничего не найдено", searchResult);
+            Console.WriteLine(searchResult);
+        }
+
+        [ClassCleanup]
+        public static void Close()
+        {
+            driver.Close();
+        }
+    }
+}
